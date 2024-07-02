75a187d7d2ac5ac92ac954933a47bfc18582aaae
Brian Mitchell <mitchelb@us.ibm.com>
2024-07-02 08:48:47 -0500

    Added code to get video resolution and format names the way I like them

--- perplex.py
+++ perplex.py
@@ -9,20 +9,69 @@
 # changes.
 
 import argparse
+import configparser
 import datetime
 import gzip
+import signal
 import json
 import os
 import shutil
 import sqlite3
 import sys
 import subprocess
-
 import progressbar as pb
+import pretty_errors
+from icecream import ic
+from plexapi.myplex import MyPlexAccount
+from plexapi.library import Library
 
 # Chars to remove from titles
 del_chars = "."
 forbiddenCharsInNames = "\\", "/", ":", "*", "?", '"', "<", ">", "|"
+
+# Configure icecream for my debugging purposes
+ic.configureOutput(prefix='DEBUG: ', includeContext=True)
+
+def signal_handler(signal_caught, frame):
+    print("\nOperations interupted. Exiting.")
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+pretty_errors.configure(
+    stack_depth=1,
+    display_locals=1,
+    lines_before=2,
+    lines_after=1,
+    display_timestamp=1,
+    timestamp_function=lambda: datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+)
+
+
+def ConnectPlex():
+    global plex
+    print("Establishing a connection to the Plex Server")
+    print("Please wait.....")
+    config = configparser.ConfigParser()
+    config.read("plex_account.ini")
+    login = config["PLEX"]["login"]
+    password = config["PLEX"]["password"]
+    server = config["PLEX"]["server"]
+    account = MyPlexAccount(login, password)
+    plex = account.resource(server).connect()  # returns a PlexServer instance
+
+def get_resolution(movie_id):
+    movie = plex.library.section("Movies").search(libtype="movie", id=movie_id)
+    if not movie:
+        resolution = "unknown"
+    else:
+        resolution =  movie[0].media[0].videoResolution
+    
+    if resolution.isnumeric():
+        return f"{resolution}p"
+    else:
+        return resolution.upper()
 
 
 def find_db(plex_dir, name):
@@ -47,14 +96,25 @@ def build_db(plex_dir, movies={}):
     db = sqlite3.connect(dbfile)
 
     # Select only movies with year
+#    query = """
+#        SELECT id, title, originally_available_at FROM metadata_items
+#        WHERE metadata_type = 1 AND originally_available_at
+#    """
     query = """
-        SELECT id, title, originally_available_at FROM metadata_items
-        WHERE metadata_type = 1 AND originally_available_at """
+        SELECT metadata_items.id, metadata_items.title, media_items.width, media_items.video_codec, media_items.audio_codec, metadata_items.originally_available_at FROM metadata_items INNER JOIN media_items ON metadata_items.id = media_items.metadata_item_id
+        WHERE metadata_items.metadata_type = 1 AND metadata_items.library_section_id = 1 AND metadata_items.originally_available_at
+    """
 
     for row in db.execute(query):
+        resolution = get_resolution(row[0])
         title = convert([x for x in row[1] if x not in del_chars])
-        year = datetime.date.fromtimestamp(row[2]).strftime("%Y")
-        movies[row[0]] = (title, year, [])
+        #print(f"Got: {resolution} for {title}")
+        width = row[2]
+        video = row[3]
+        audio = row[4]
+        year = datetime.date.fromtimestamp(row[5]).strftime("%Y")
+        #movies[row[0]] = (title, year, [])
+        movies[row[0]] = (title, width, video, audio, year, resolution, [])
 
     # Get files for each movie
     query = """
@@ -65,7 +125,8 @@ def build_db(plex_dir, movies={}):
     for id in movies:
         try:
             for file in db.execute(query % id):
-                movies[id][2].append(file[0])
+                #movies[id][2].append(file[0])
+                movies[id][6].append(file[0])
                 files += 1
         except Exception as e:
             errorOut(e)
@@ -105,7 +166,8 @@ def windows_to_wsl_path(windows_path):
 def build_map(movies, dest, printDoubles, directoryToRunOn="", mapping=[]):
     """Build mapping to new names"""
 
-    for title, year, files in list(movies.values()):
+    #for title, year, files in list(movies.values()):
+    for title, width, video, audio, year, resolution, files in list(movies.values()):
         counter = 0
         for i, old_name in enumerate(files):
             old_name = windows_to_wsl_path(old_name)
@@ -118,8 +180,9 @@ def build_map(movies, dest, printDoubles, directoryToRunOn="", mapping=[]):
 
             # BRM don't sub-dir
             #template = "%s (%s)/%s (%s)" % (title, year, title, year)
-            template = "%s (%s)" % (title, year)
-            template += " - part%d" % (i + 1) if len(files) > 1 else ""
+            #template = "%s (%s)" % (title, year)
+            template = "%s (%s)-%s.%s.%s" % (title, year, video, resolution, audio)
+            template += "-part%d" % (i + 1) if len(files) > 1 else ""
             template += ext
             template = "_".join(template.split())
 
@@ -229,6 +292,8 @@ if __name__ == "__main__":
         args.dest is not None
     ):
         errorOut("Cant provide --dest and --justRename Args at the same time")
+
+    ConnectPlex()
 
     if args.plex:
         movies = build_db(args.plex)
